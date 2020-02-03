ce2e847d549fea543faa15cc5f163d421e506945
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-02-03 17:58:04 -0300

    runtime error!

---
+++ uri_1061_basic.py
@@ -0,0 +1,31 @@
+d_i = int(input("Dia "))
+v_inicio = input().split(" : ")
+h1,m1,s1 = list(map(int,v_inicio))
+d_f = int(input("Dia "))
+v_final = input ().split(" : ")
+h2,m2,s2 = list(map(int, v_final))
+
+d = d_f - d_i
+
+h = h2 - h1
+if (h < 0):
+    h = 24 + h
+    d = d -1
+
+m = m2 - m1
+if(m < 0):
+    m = 60 + m
+    h = h -1
+
+s = s2 - s1
+if(s < 0):
+    s = 60 + s
+    h = h - 1
+
+if(d <= 0):
+    d = 0
+
+print("%d dia (s)" %d)
+print("%d hora (s)" %h)
+print("%d minuto (s)" %m)
+print("%d segundo (s)" %s)
