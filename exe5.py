eed788829fe7bd0686d33443b80988711ffc33f4
Marcelo Claro de Souza <marcelo.claro.souza@gmail.com>
2020-02-18 10:39:59 -0300

    Basic exercices - Learn Python3

---
+++ exe5.py
@@ -0,0 +1,12 @@
+my_name = 'zed shaw'
+my_age = 35
+my_height = 74
+my_weight = 180
+my_eyes = 'blue'
+my_teeth = 'white'
+my_hair = 'brown'
+
+
+print(f"let's talk about {my_name}.")
+print(f"he's {my_height} inches tall")
+print(f"he's {my_weight} pounds heavy")
---
+++ exe7.py
@@ -0,0 +1,11 @@
+print("Mary had a little lamb.")
+print("its fleece was white as {}.".format('snow'))
+print("and everywhere that Mary went.")
+print("." * 10)
+
+end1 = "a"
+end2 = "b"
+
+print(end1 + end2)#print end1 and end2 without a space 
+print(end1, end = " ")#print a space between end1 and end2
+print(end2)
