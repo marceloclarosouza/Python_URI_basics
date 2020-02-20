f948a83e3c4fb2c44e9fb24c3ba48c3967a49c2f
Marcelo Claro de Souza <marcelo.claro.souza@gmail.com>
2020-02-20 09:53:12 -0300

    basic concepts of Python3

---
+++ exe11.py
@@ -0,0 +1,8 @@
+print("How old are you?", end = ' ') ##the use of end = ' ' tell to print to not end with a newline character and go to the next line
+age = input()
+print("How tall are you?", end = ' ')
+height = input()
+print("How much do you weight?", end = ' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
---
+++ exe13_parameters_unpacking_variables.py
@@ -0,0 +1,10 @@
+## open with terminal using python3.6 exe13_parameters_unpacking_variables.py first second third
+
+from sys import argv
+script, first, second, third = argv
+
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Yur third variable is:", third)
